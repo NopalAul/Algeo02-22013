abe0849b91ef23f044ab29c7b7c0e34f7fc280df
denoseu <denisefeliciatiowanni@gmail.com>
2023-11-04 13:57:26 +0700

    Update glcm.py

--- src/glcm.py
+++ src/glcm.py
@@ -1,6 +1,7 @@
 import numpy as np
+from skimage.feature import graycomatrix
 
-def create_glcm_matrix(image, d=1, angle=0):
+def matrixGLCM(image, d=1, angle=0):
     # quantization levels
     levels = np.max(image) + 1
 
@@ -19,11 +20,11 @@ def create_glcm_matrix(image, d=1, angle=0):
                 if angle == 0:
                     x2, y2 = i + 0, j + d
                 elif angle == 90:
-                    x2, y2 = ...
+                    x2, y2 = i + d, j + 0
                 elif angle == 45:
-                    x2, y2 = ...
+                    x2, y2 = i + d, j + d
                 elif angle == 135:
-                    x2, y2 = ...
+                    x2, y2 = i + 0, j + d
 
                 if 0 <= x1 < rows and 0 <= y1 < cols and 0 <= x2 < rows and 0 <= y2 < cols:
                     pixel1 = image[x1, y1]
@@ -33,12 +34,12 @@ def create_glcm_matrix(image, d=1, angle=0):
     return framework_matrix
 
 # bikin matriks simetriknya
-def create_symmetric_glcm(glcm_matrix):
+def symmetricGLCM(glcm_matrix):
     symmetric_glcm = glcm_matrix + glcm_matrix.T
     return symmetric_glcm
 
 # normalisasi matriks
-def normalize_glcm(glcm_matrix):
+def normalizeGLCM(glcm_matrix):
     normalized_glcm = glcm_matrix / np.sum(glcm_matrix)
     return normalized_glcm
 
@@ -52,9 +53,18 @@ image = np.array([[0, 0, 1],
 #                   [4, 5, 7, 1, 2],
 #                   [8, 5, 1, 2, 5]])
 
-glcm_matrix = create_glcm_matrix(image, d=1, angle=0)
-symmetric_glcm = create_symmetric_glcm(glcm_matrix)
-normalized_glcm = normalize_glcm(symmetric_glcm)
+glcm_matrix = matrixGLCM(image, d=1, angle=135)
+symmetric_glcm = symmetricGLCM(glcm_matrix)
+normalized_glcm = normalizeGLCM(symmetric_glcm)
+
+# glcm = graycomatrix(image, 
+#                     distances=[1], 
+#                     angles=[135], 
+#                     levels=4,
+#                     symmetric=True, 
+#                     normed=True)
+
+# print(glcm[:,:,0,0])
 
 print("GLCM Matrix:")
 print(glcm_matrix)
