af6760a0784ebf748de81a17cd340fcd5efd65ef
b33rk <brkmanics@gmail.com>
2023-11-05 15:40:58 +0700

    fix: param_warna (cara2, rgbToHistogram)

--- src/nyoba/param_warna.py
+++ src/nyoba/param_warna.py
@@ -1,4 +1,6 @@
 import colorsys
+import cv2
+import numpy as np
 from PIL import Image
 from cosine_similarity import *
 
@@ -13,7 +15,6 @@ else:
 ar = array(img)
 ar1 = array(img1)
 
-print(ar[0][0], ar1[0][0])
 def cara1(m1, m2, row, col):
     sum = 0
     c = 0
@@ -25,53 +26,50 @@ def cara1(m1, m2, row, col):
             sum += cosine_sim(l,l1)
             c += 1
     return sum/c
-
+    
 def cara2(m1, m2, row, col):
-    sum = 0
-    c = 0
-    # row -= row % 3
-    # col -= col % 3
+    x = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    y = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
     for i in range(0,row,3):
         for j in range(0,col,3):
-            l = rgbToHistogram(m1[i][j][0],m1[i][j][1],m1[i][j][2])
-            l1 = rgbToHistogram(m2[i][j][0],m2[i][j][1],m2[i][j][2])
-            sum += cosine_sim(l,l1)
-            c += 1
-    print("c: ", c) #########
-    return sum/c
+            x = rgbToHistogram(m1[i][j][0],m1[i][j][1],m1[i][j][2],x)
+            y = rgbToHistogram(m2[i][j][0],m2[i][j][1],m2[i][j][2],y)
+    return cosine_sim(x,y)
 
-def hsvtohistogram(h,s,v):
+def hsvtohistogram(h,s,v,l):
     if h >= 316:
-        h = 0
+        l[0] += 1
     elif h <= 25:
-        h = 1
+        l[1] += 1
     elif h <= 40:
-        h = 2
+        l[2] += 1
     elif h <= 120:
-        h = 3
+        l[3] += 1
     elif h <= 190:
-        h = 4
+        l[4] += 1
     elif h <= 270:
-        h = 5
+        l[5] += 1
     elif h <= 295:
-        h = 6
+        l[6] += 1
     elif h <= 315:
-        h = 6
+        l[7] += 1
     if s < 0.2:
-        s = 0
+        l[8] += 1
     elif s < 0.7:
-        s = 1
+        l[9] += 1
     elif s >= 0.7:
-        s = 2
+        l[10] += 1
     if v < 0.2:
-        v = 0
+        l[11] += 1
     elif v < 0.7:
-        v = 1
+        l[12] += 1
     elif v >= 0.7:
-        v = 2
-    return [h,s,v]
+        l[13] += 1
+    return l
 
-def rgbToHistogram(r,g,b):
+
+def rgbToHistogram(r,g,b,l):
     # normalisasi
     r = r/255
     g = g/255
@@ -99,6 +97,32 @@ def rgbToHistogram(r,g,b):
         s = 0
     ## V
     v = cmax
-    return hsvtohistogram(h,s,v)
+    return hsvtohistogram(h,s,v,l)
 
 print(f"cosine similarity : {cara2(ar,ar1, img.height, img.width)}")
+# def hsvtohistogram2(imgg):
+#     row = imgg.height
+#     col = imgg.width
+#     img = array(imgg)
+#     for i in range(0,row,3):
+#         for j in range(0,col,3):
+#             img[i][j] = rgbToHistogram(img[i][j][0],img[i][j][1],img[i][j][2])
+#     hist = cv2.calcHist(img, [0, 1, 2], None, [180,256,256], [0, 180, 0, 256, 0, 256])
+#     hist = cv2.normalize(hist, hist)
+#     return hist.flatten()
+
+# def cara3(m1):
+#     for i in range(len(m1)):
+#         for j in range(len(m1)):
+#             m1[i][j] = rgbToHistogram(m1[i][j][0],m1[i][j][1],m1[i][j][2])
+#     return m1
+
+# def cara4(ar):
+#     hsv_histogram = HSVColorHistogram([8, 8, 8])
+#     source_color_features = hsv_histogram.describe(ar)
+#     source_color_features = np.around(np.array(source_color_features, dtype=np.float32), decimals=8)
+#     return source_color_features
+
+# print(cosine_sim(cara4(ar), cara4(ar1)))
+
+# print(cara3(hsvtohistogram2(img), hsvtohistogram2(img1)))
