4e57717ed98b4016f60d462ddf980bca2fc01723
denoseu <denisefeliciatiowanni@gmail.com>
2023-11-11 16:56:44 +0700

    belajar pagination

--- src/website/src/App.js
+++ src/website/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/header";
 import SearchField from "./components/searchfield";
 import DoraButtonPage from "./components/dora-button-page";
 import NobiButtonPage from "./components/nobi-button-page";
+import Images from "./components/images";
 
 function App() {
   const handleButtonClick = () => {
@@ -12,9 +13,15 @@ function App() {
   return (
     <Router>
         <Routes>
-          <Route path="/" element={<Header onButtonClick={handleButtonClick}>
-            <SearchField/>
-          </Header>}>
+          <Route 
+            path="/" 
+            element={
+              <div>
+                <Header onButtonClick={handleButtonClick}>
+                  <SearchField/>
+              </Header>
+              <Images/>
+            </div> }>
           </Route>
           <Route path="/buttonpage" element={<DoraButtonPage />} />
           <Route path="/nobi-button-page" element={<NobiButtonPage />} />
---
+++ src/website/src/components/images.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import suneo from './rsc/suneo.png'; 
+
+const Images = () => {
+    const [images, setImages] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [currentPage, setCurrentPage] = useState(1);
+    const imagesPerPage = 8;
+
+    useEffect(() => {
+        const fetchImages = async () => {
+            try {
+                const imagePaths = [
+                    suneo, suneo, suneo, suneo, suneo, suneo, suneo, suneo, suneo, suneo,
+                    suneo, suneo, suneo, suneo, suneo, suneo, suneo, suneo, suneo, suneo,
+                    suneo, suneo
+                ];
+
+                const imageObjects = imagePaths.map((path) => ({ url: path }));
+                setImages(imageObjects);
+            } catch (error) {
+                console.error('Error fetching images:', error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchImages();
+    }, []);
+
+    const indexOfLastImage = currentPage * imagesPerPage;
+    const indexOfFirstImage = indexOfLastImage - imagesPerPage;
+    const currentImages = images.slice(indexOfFirstImage, indexOfLastImage);
+
+    const paginate = (pageNumber) => {
+        if (pageNumber > 0 && pageNumber <= Math.ceil(images.length / imagesPerPage)) {
+            setCurrentPage(pageNumber);
+        }
+    };
+
+    return (
+        <>
+            <div className="flex justify-center items-center">
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 my-10 max-w-7xl mx-auto px-4">
+                    {isLoading ? (
+                        Array(imagesPerPage).fill(null).map((_, index) => (
+                            <Skeleton key={index} height={200} width={'100%'} />
+                        ))
+                    ) : (
+                        currentImages.map((image, index) => (
+                            <img
+                                key={index}
+                                src={image.url}
+                                alt=""
+                                style={{ width: '80%', height: '100%', objectFit: 'cover' }}
+                            />
+                        ))
+                    )}
+                </div>
+            </div>
+            {/* pagination */}
+            <div className='flex justify-center my-4 pb-4'>
+                {Array(Math.ceil(images.length / imagesPerPage))
+                    .fill(null)
+                    .map((_, index) => (
+                        <button
+                            key={index}
+                            onClick={() => paginate(index + 1)}
+                            className={`mx-2 p-2 focus:outline-none rounded-full ${currentPage === index + 1 ? 'bg-sky-800 text-white' : 'bg-sky-200 text-sky-700'}`}
+                        >
+                            {index + 1}
+                        </button>
+                    ))}
+            </div>
+        </>
+    );
+};
+
+export default Images;
